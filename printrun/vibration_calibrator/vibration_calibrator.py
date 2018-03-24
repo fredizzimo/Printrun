274200b7f818b503cca3e882bd4a455af2c6b495
Fred Sundvik <fsundvik@gmail.com>
2018-03-24 23:29:50 +0200

    Add simple gui and test command

--- printrun/vibration_calibrator/vibration_calibrator.py
+++ printrun/vibration_calibrator/vibration_calibrator.py
@@ -1,4 +1,11 @@
+from printrun import pronsole
+
+
 class VibrationCalibrator(object):
 
     def __init__(self, parent):
-        self.parent = parent
+        self.parent = parent # type: pronsole.pronsole
+
+    def start_analysis(self):
+        if self.parent.p.online:
+            self.parent.p.send_now("G28") # Home all axis
--- printrun/vibration_calibrator/vibration_calibrator_gui.py
+++ printrun/vibration_calibrator/vibration_calibrator_gui.py
@@ -6,3 +6,26 @@ class VibrationCalibratorMainWindow(wx.Frame):
         wx.Frame.__init__(self, parent,
                           title = "Vibration Calibrator",
                           style = wx.DEFAULT_FRAME_STYLE | wx.FRAME_FLOAT_ON_PARENT)
+
+        self.vibration_calibrator = vibration_calibrator
+
+        self.stop_button = wx.Button(parent=self, label="Stop")
+
+        self.start_analysis_button = wx.Button(parent=self, label="Start")
+
+        self.analyzis_sizer = wx.StaticBoxSizer(orient=wx.VERTICAL, parent=self, label="Analysis")
+        self.analyzis_sizer.Add(self.start_analysis_button)
+
+
+        self.full_sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        self.full_sizer.Add(self.stop_button)
+        self.full_sizer.Add(self.analyzis_sizer)
+        self.full_sizer.SetSizeHints(self)
+
+
+        self.SetSizer(self.full_sizer)
+
+        self.start_analysis_button.Bind(wx.EVT_BUTTON, self.onClickStartAnalysis)
+
+    def onClickStartAnalysis(self, event):
+        self.vibration_calibrator.start_analysis()
